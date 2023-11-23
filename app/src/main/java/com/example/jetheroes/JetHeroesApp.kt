98ed73b1bbcb4c31e0741507268832c67cf57ec0
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2023-11-23 11:52:10 +0700

    tambah fitur search

--- app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
@@ -1,16 +1,19 @@
 package com.example.jetheroes
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,6 +22,8 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -54,6 +59,7 @@ fun JetHeroesApp(
     viewModel: JetHeroesViewModel = viewModel(factory = ViewModelFactory(HeroRepository()))
 ) {
     val groupedHeroes by viewModel.groupedHeroes.collectAsState()
+    val query by viewModel.query
 
     //cara old
 //    val groupedHeroes=HeroesData.heroes
@@ -72,6 +78,13 @@ fun JetHeroesApp(
             state = listState,
             contentPadding = PaddingValues(bottom = 80.dp)
         ) {
+            item{
+                SearchBar(
+                    query = query,
+                    onQueryChange = viewModel::search,
+                    modifier=Modifier.background(MaterialTheme.colorScheme.primary)
+                )
+            }
             groupedHeroes.forEach { (initial, heroes) ->
                 stickyHeader {
                     CharacterHeader(initial)
@@ -80,7 +93,7 @@ fun JetHeroesApp(
                     HeroListItem(
                         name = hero.name,
                         photoUrl = hero.photoUrl,
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier.fillMaxWidth().animateItemPlacement(tween(300))
                     )
                 }
             }
@@ -171,6 +184,35 @@ fun CharacterHeader(
                 .fillMaxWidth()
                 .padding(8.dp)
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    androidx.compose.material3.SearchBar(
+        query = query,
+        onQueryChange = onQueryChange,
+        onSearch = {},
+        active = false,
+        onActiveChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        },
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+    ){
+
     }
 }
 
--- app/src/main/java/com/example/jetheroes/JetHeroesViewModel.kt
+++ app/src/main/java/com/example/jetheroes/JetHeroesViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.jetheroes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.jetheroes.data.HeroRepository
@@ -7,23 +9,33 @@ import com.example.jetheroes.model.Hero
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class JetHeroesViewModel(private val repo :HeroRepository):ViewModel() {
-    private val _groupedHeroes= MutableStateFlow(
+class JetHeroesViewModel(private val repo: HeroRepository) : ViewModel() {
+    private val _groupedHeroes = MutableStateFlow(
         repo.getHeroes()
             .sortedBy { it.name }
             .groupBy { it.name[0] }
     )
 
-    val groupedHeroes:StateFlow<Map<Char,List<Hero>>> get() = _groupedHeroes
+    val groupedHeroes: StateFlow<Map<Char, List<Hero>>> get() = _groupedHeroes
+
+    private val _query = mutableStateOf("")
+    val query: State<String> get() = _query
+
+    fun search(newQuery: String) {
+        _query.value = newQuery
+        _groupedHeroes.value = repo.searchHeroes(_query.value)
+            .sortedBy { it.name }
+            .groupBy { it.name[0] }
+    }
 }
 
-class ViewModelFactory(private val repo:HeroRepository):ViewModelProvider.NewInstanceFactory(){
+class ViewModelFactory(private val repo: HeroRepository) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if(modelClass.isAssignableFrom(JetHeroesViewModel::class.java)){
+        if (modelClass.isAssignableFrom(JetHeroesViewModel::class.java)) {
             return JetHeroesViewModel(repo) as T
         }
-        throw IllegalArgumentException("Unknown ViewModel class"+modelClass.name)
+        throw IllegalArgumentException("Unknown ViewModel class" + modelClass.name)
     }
 }
